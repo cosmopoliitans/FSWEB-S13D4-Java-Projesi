b06d5bd19a727c0491f0d0fb3844105026301ee6
cosmopoliitans <elfkyn52@gmail.com>
2023-07-28 15:33:59 +0300

    Encapculation

---
+++ src/com/workintech/model/Point.java
@@ -0,0 +1,46 @@
+package com.workintech.model;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double distance(Point p){
+        if(p != null) {
+            int xDiff = this.x - p.getX();
+            int yDiff = this.y - p.getY();
+            return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+            return distance();
+    }
+
+    public double distance(int a, int b){
+        int xDiff = this.x - a;
+        int yDiff = this.y - b;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+}
+
